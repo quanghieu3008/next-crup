8b5bcd19fa7821381964375818e702b1775b0cd3
quang <taan300897@gmail.com>
2021-01-27 11:37:20 +0700

    pussh

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
+import React from 'react';
 import '../public/global.css';
-import App from 'next/app';
 import { Provider } from 'react-redux';
 import { createWrapper } from 'next-redux-wrapper';
 import store from '../store/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Auth0Provider } from "@auth0/auth0-react";
-import '../styles/index.css'
+// import '../styles/index.css';
 import Header from './header';
-import { useAuth0 } from "@auth0/auth0-react";
+
 const MyApp = ({ Component, pageProps }) => {
 
     return (
